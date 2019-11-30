c8baf782bad4045f0a13850da9d4fd4225307bbe
Parth Chugh <parthchugh52@gmail.com>
2019-11-30 19:24:42 +0530

    added posts page

---
+++ app/javascript/Routes.js
@@ -0,0 +1,35 @@
+// app/javascript/routes.js
+import React from 'react';
+import {
+  Switch,
+  Route,
+} from "react-router-dom";
+import CreatePost from './bundles/posts/CreatePost';
+import UpdatePost from './bundles/posts/UpdatePost';
+import PostDetails from './bundles/posts/PostDetails';
+import Posts from './bundles/posts/index';
+
+export default () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Posts />
+      </Route>
+      <Route
+        path="/posts/:id"
+        exact
+        component={PostDetails}
+       />
+      <Route
+        path="/posts/new"
+        exact
+        component={CreatePost}
+      />
+      <Route
+        path="/posts/:id/edit"
+        exact
+        component={UpdatePost}
+        />
+    </Switch>
+  );
+}
